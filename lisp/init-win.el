1787e299c71125550698195b47a42d75a0858e5b
welandx <weland@bupt.edu.cn>
2023-07-13 22:09:17 +0800

    exec-path config

--- lisp/init-win.el
+++ lisp/init-win.el
@@ -1,6 +1,7 @@
-(ignore-errors (module-load (expand-file-name"~/.emacs.d/site-lisp/pop_select.dll")))
-(pop-select/transparent-set-all-frame 235)
-(setq find-program "c:/Users/zwxxm/scoop/apps/msys2/2023-03-18/usr/bin/find.exe")
-(add-to-list 'exec-path "c:/Users/zwxxm/scoop/apps/msys2/2023-03-18/usr/bin/")
-
+;; (ignore-errors (module-load (expand-file-name"~/.emacs.d/site-lisp/pop_select.dll")))
+;; (pop-select/transparent-set-all-frame 235)
+;; (setq find-program "c:/Users/zwxxm/scoop/apps/msys2/2023-03-18/usr/bin/find.exe")
+;; (add-to-list 'exec-path "c:/Users/zwxxm/scoop/apps/msys2/2023-03-18/usr/bin/")
+(add-to-list 'exec-path "/cygdrive/c/Users/Administrator/AppData/Local/Microsoft/WinGet/Links/")
+(add-to-list 'exec-path "c:/Users/Administrator/AppData/Local/Microsoft/WinGet/Links/")
 (provide 'init-win)
