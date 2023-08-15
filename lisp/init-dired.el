07ea0e73f301db63fae07784ddc19d135ab4e0ba
welandx <weland@bupt.edu.cn>
2023-08-15 10:40:50 +0800

    dired for mac, may break win or linux

--- lisp/init-dired.el
+++ lisp/init-dired.el
@@ -1,6 +1,8 @@
 (use-package dired
   :demand t
   :hook (dired-mode . dired-hide-details-mode)
+  :config
+  (setq insert-directory-program "gls" dired-use-ls-dired t)
   :custom
   (dired-dwim-target t)
   (dired-listing-switches "-alGhv --group-directories-first")
