46acc4ad8a78ceabb5b3df3c89ab6cde8743211a
welandx <weland@bupt.edu.cn>
2023-08-22 10:47:04 +0800

    disable cursor

--- lisp/init-win.el
+++ lisp/init-win.el
@@ -4,25 +4,25 @@
 ;; (add-to-list 'exec-path "c:/Users/zwxxm/scoop/apps/msys2/2023-03-18/usr/bin/")
 (add-to-list 'exec-path "/cygdrive/c/Users/Administrator/AppData/Local/Microsoft/WinGet/Links/")
 (add-to-list 'exec-path "c:/Users/Administrator/AppData/Local/Microsoft/WinGet/Links/")
-(when (fboundp 'pop-select/beacon-animation)
-  (add-hook 'post-command-hook (lambda()
-                                 (ignore-errors
-                                   (let* ((p (window-absolute-pixel-position))
-                                          (pp (point))
-                                          (w (if (equal cursor-type 'bar) 1
-                                               (if-let ((glyph (when (< pp (point-max))
-                                                                 (aref (font-get-glyphs (font-at pp) pp (1+ pp)) 0))))
-                                                   (aref glyph 4)
-                                                 (window-font-width))))
-                                          (h (line-pixel-height))
-                                          )
-                                     (when p
-                                       (pop-select/beacon-animation (car p) ; x
-                                                                    (cdr p) ; y
-                                                                    w
-                                                                    h
-                                                                    100 ; timer
-                                                                    50 ; timer step
-                                                                    233 86 120 ; r g b
-                                                                    )))))))
+;; (when (fboundp 'pop-select/beacon-animation)
+;;   (add-hook 'post-command-hook (lambda()
+;;                                  (ignore-errors
+;;                                    (let* ((p (window-absolute-pixel-position))
+;;                                           (pp (point))
+;;                                           (w (if (equal cursor-type 'bar) 1
+;;                                                (if-let ((glyph (when (< pp (point-max))
+;;                                                                  (aref (font-get-glyphs (font-at pp) pp (1+ pp)) 0))))
+;;                                                    (aref glyph 4)
+;;                                                  (window-font-width))))
+;;                                           (h (line-pixel-height))
+;;                                           )
+;;                                      (when p
+;;                                        (pop-select/beacon-animation (car p) ; x
+;;                                                                     (cdr p) ; y
+;;                                                                     w
+;;                                                                     h
+;;                                                                     100 ; timer
+;;                                                                     50 ; timer step
+;;                                                                     233 86 120 ; r g b
+;;                                                                     )))))))
 (provide 'init-win)
