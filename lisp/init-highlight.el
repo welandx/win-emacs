ad99a52d290e62ba4815073dce1c1caab53ad116
weland <weland@bupt.edu.cn>
2023-12-22 00:00:19 +0800

    move treesit & hl-defined to init-highlight

---
+++ lisp/init-highlight.el
@@ -0,0 +1,15 @@
+(use-package treesit-auto
+  :ensure t
+  :config
+  (treesit-auto-add-to-auto-mode-alist '("cpp" "yaml"))
+  (global-treesit-auto-mode)
+  (setq treesit-font-lock-level 4))
+
+(use-package hl-defined
+  ;; 高亮 emacs-lisp function
+  :if *is-a-mac*
+  :load-path "./site-lisp/hl-defined"
+  :config
+  (add-hook 'emacs-lisp-mode-hook 'hdefd-highlight-mode 'APPEND))
+
+(provide 'init-highlight)
