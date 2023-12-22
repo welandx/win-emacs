5cf1c971abd0140ad45e298ba94d7b225ff779dd
weland <weland@bupt.edu.cn>
2023-12-22 13:08:51 +0800

    snippet | treesit hl in org-src-block

--- lisp/init-highlight.el
+++ lisp/init-highlight.el
@@ -11,4 +11,13 @@
   :config
   (add-hook 'emacs-lisp-mode-hook 'hdefd-highlight-mode 'APPEND))
 
+;; 在 org-src-block 使用 ts-mode 高亮
+(with-eval-after-load 'org
+  (defun my/remap-mode (mode)
+    "make org-src-get-lang-mode respect major-mode-remap-alist"
+    (treesit-auto--set-major-remap)
+    (alist-get mode major-mode-remap-alist mode))
+  (advice-add 'org-src-get-lang-mode :filter-return #'my/remap-mode))
+
+
 (provide 'init-highlight)
