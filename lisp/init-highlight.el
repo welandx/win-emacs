184ed5414181fd36ad490a5a09b94bf11a73f451
welandx <weland@bupt.edu.cn>
2024-01-05 01:36:13 +0800

    drop hl-defined, replaced with highlight-defined

--- lisp/init-highlight.el
+++ lisp/init-highlight.el
@@ -5,12 +5,6 @@
   (global-treesit-auto-mode)
   (setq treesit-font-lock-level 4))
 
-(use-package hl-defined
-  ;; 高亮 emacs-lisp function
-  :load-path "./site-lisp/hl-defined"
-  :config
-  (add-hook 'emacs-lisp-mode-hook 'hdefd-highlight-mode 'APPEND))
-
 ;; 在 org-src-block 使用 ts-mode 高亮
 (with-eval-after-load 'org
   (defun my/remap-mode (mode)
@@ -19,5 +13,9 @@
     (alist-get mode major-mode-remap-alist mode))
   (advice-add 'org-src-get-lang-mode :filter-return #'my/remap-mode))
 
+(use-package highlight-defined
+  :ensure t
+  :hook
+  (emacs-lisp-mode . highlight-defined-mode))
 
 (provide 'init-highlight)
