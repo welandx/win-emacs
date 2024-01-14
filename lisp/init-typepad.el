7156f4b754dfeb637dd8d252e5914a32c27f96b1
weland <weland@bupt.edu.cn>
2024-01-14 21:07:27 +0800

    visual-fill-mode in typepad

--- lisp/init-typepad.el
+++ lisp/init-typepad.el
@@ -4,3 +4,8 @@
 
 (setq typepad-text-path (concat user-emacs-directory "site-lisp/TypePad/top500.txt"))
 
+;; enable visual-fill-column-mode in typepad-mode and typepad-readonly-mode
+(add-hook 'typepad-mode-hook 'visual-fill-column-mode)
+(add-hook 'typepad-readonly-mode-hook 'visual-fill-column-mode)
+
+
