94d6e6ff77b2a8e7ff436f6a7695d55f7ca297f8
welandx <weland@bupt.edu.cn>
2024-01-29 23:33:04 +0800

    disable list-switches for mac

--- lisp/init-dired.el
+++ lisp/init-dired.el
@@ -7,7 +7,7 @@
    (setq insert-directory-program "gls" dired-use-ls-dired t))
   :custom
   (dired-dwim-target t)
-  (dired-listing-switches "-alGhv --group-directories-first")
+  ;; (dired-listing-switches "-alGhv --group-directories-first")
   (dired-recursive-copies 'always)
   (dired-kill-when-opening-new-dired-buffer t))
 
